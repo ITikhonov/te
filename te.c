9ebf8f467e6550dd13b69901c27aa7b0b012ec8c
Ivan Tikhonov <kefeer@brokestream.com>
2010-08-27 17:58:45 +0600

    hsl and h line

--- te.c
+++ te.c
@@ -7,6 +7,41 @@ SDL_Surface *tile;
 SDL_Surface *sc;
 
 int ccolor=0;
+int ch,cs,cl;
+
+// http://qscribble.blogspot.com/2008/06/integer-conversion-from-hsl-to-rgb.html
+uint32_t HSL(int hue, int sat, int lum)
+{
+    int v,r,g,b;
+
+    v = (lum < 128) ? (lum * (256 + sat)) >> 8 :
+          (((lum + sat) << 8) - lum * sat) >> 8;
+    if (v <= 0) {
+        r = g = b = 0;
+    } else {
+        int m;
+        int sextant;
+        int fract, vsf, mid1, mid2;
+
+        m = lum + lum - v;
+        hue *= 6;
+        sextant = hue >> 8;
+        fract = hue - (sextant << 8);
+        vsf = v * fract * (v - m) / v >> 8;
+        mid1 = m + vsf;
+        mid2 = v - vsf;
+        switch (sextant) {
+           case 0: r = v; g = mid1; b = m; break;
+           case 1: r = mid2; g = v; b = m; break;
+           case 2: r = m; g = v; b = mid1; break;
+           case 3: r = m; g = mid2; b = v; break;
+           case 4: r = mid1; g = m; b = v; break;
+           case 5: r = v; g = m; b = mid2; break;
+        }
+    }
+    return SDL_MapRGBA(sc->format,r,g,b,0xff);
+}
+
 
 int main(int argc, char *argv[]) {
 	SDL_Init(SDL_INIT_VIDEO);
@@ -81,6 +116,17 @@ int main(int argc, char *argv[]) {
 				SDL_FillRect(sc,&s,SDL_MapRGBA(sc->format,r,g,b,a));
 			}
 
+		}
+
+		{
+			SDL_Rect a={256+32,256+32,256,256};
+			SDL_FillRect(sc,&a,0xffffff);
+
+			SDL_Rect r={256+32,256+32,1,32};
+			int i; for(i=0;i<256;i++) {
+				SDL_FillRect(sc,&r,HSL(i,255,128));
+				r.x++;
+			}
 		}
 
 		if(x<256 && y<256) {
